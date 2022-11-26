2ed425212013a1c82325f5300dfe7b3dda3303de
ShamimUddin <Shamim.Uddin@skytek.com>
2022-11-26 14:26:06 +0600

    first commit

---
+++ WebApplication3/Controllers/HTMLPDFController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SelectPdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Model;
+
+namespace WebApplication3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HTMLPDFController : ControllerBase
+    {
+        [HttpGet]
+        public dynamic GetPDFTOHTML(ModelClass obj)
+        {
+            HtmlToPdf converter = new HtmlToPdf();
+            PdfDocument doc = converter.ConvertUrl("file:///C:/Users/Hp/Desktop/index.html");
+            doc.Save($@"{AppDomain.CurrentDomain.BaseDirectory}\url.pdf");
+            doc.Close();
+            return Ok();
+
+            //---------Need to convert JSON Stringify
+            //PdfDocument doc = converter.ConvertHtmlString(obj.stringData);
+            //byte[] pdffile = doc.Save();
+            //doc.Close();  
+            //string base64String = Convert.ToBase64String(pdffile, 0, pdffile.Length);
+            //return Ok(base64String);
+
+
+
+        }
+    }
+}
---
+++ WebApplication3/Model/ModelClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Model
+{
+    public class ModelClass
+    {
+        public string stringData { set; get; }
+    }
+}
